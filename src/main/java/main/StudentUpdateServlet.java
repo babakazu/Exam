450615ba4fc37ac5e54909182a4fc7bd52987016
kou <oky2384930@stu.o-hara.ac.jp>
2024-04-24 12:28:42 +0900

    a

---
+++ src/main/java/main/StudentUpdateServlet.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.io.IOException;
+
+import dao.StudentDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/main/StudentUpdateServlet")
+public class StudentUpdateServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String no = request.getParameter("no");
+        String entYear = request.getParameter("entYear");
+        String name = request.getParameter("name");
+        String classNum = request.getParameter("classNum");
+        String isAttend = request.getParameter("isAttend");
+
+        // TODO: DAO を使って学生情報を更新する処理を記述
+        StudentDAO studentDAO = new StudentDAO();
+        studentDAO.updateStudent(no, entYear, name, classNum, isAttend);
+
+        // 学生一覧にリダイレクト
+        response.sendRedirect("http://localhost:8080/test/tokuten/student_updatesucces.jsp");
+    }
+}
---
+++ src/main/webapp/tokuten/student_updatesucces.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@page contentType="text/html; charset=UTF-8" %>
+<%@ include file="../header.jsp" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>学生一覧</title>
+    <style>
+        /* スタイルは省略 */
+    </style>
+</head>
+<body>
+    <h2>学生管理</h2>
+
+    <div class="menu-links">
+        <a href="student_insert.jsp">学生登録</a>
+        <a href="student_update.jsp">学生変更</a>
+        <a href="../grades/grades_insert.jsp">成績登録</a>
+        <a href="../grades/grades_list.jsp">成績参照</a>
+        <a href="../subject/subject_list.jsp">科目管理</a>
+        <a href="../login/login-out.jsp">ログアウト</a>
+    </div>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>学生変更完了</title>
+</head>
+<body>
+    <h2>学生変更完了</h2>
+    <p>学生情報が正常に変更されました。</p>
+    <a href="student_list.jsp">学生一覧に戻る</a>
+</body>
+</html>
